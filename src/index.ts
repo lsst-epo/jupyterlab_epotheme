be5fa37518d7cb126ec1a2311cee09a6d6800d11
Abbey Yacoe <ayacoe@lsst.org>
2018-06-25 11:08:53 -0700

    working skeleton

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+
+import {
+  JupyterLab, JupyterLabPlugin
+} from '@jupyterlab/application';
+
+import {
+  IThemeManager
+} from '@jupyterlab/apputils';
+
+
+/**
+ * A plugin for the jupyterlab_epotheme
+ */
+const plugin: JupyterLabPlugin<void> = {
+  id: 'jupyterlab_epotheme:plugin',
+  requires: [IThemeManager],
+  activate: function(app: JupyterLab, manager: IThemeManager) {
+    manager.register({
+      name: 'jupyterlab_epotheme',
+      load: function() {
+        return manager.loadCSS('jupyterlab_epotheme/index.css');
+      },
+      unload: function() {
+        return Promise.resolve(void 0);
+      }
+    });
+  },
+  autoStart: true
+};
+
+
+export default plugin;
